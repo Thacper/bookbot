5fb742e4185ae24a2cb6f279c0a34decd616e6fb
Thacper <timo.perhac@gmail.com>
2025-05-17 22:37:39 +0200

    v1.0: Bookbot upload

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
+import sys
+if len(sys.argv)!=2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    path = sys.argv[1]
 
-from stats import get_num_words
-words_count=get_num_words()
-print(f"{words_count} words found in the document")
+
+#import functions from stats
+import stats
+
+book_text = stats.get_book_text(path)
+words_count = stats.get_num_words(book_text)
+char_dict = stats.get_num_chars(book_text)
+
+chars_sorted_list = stats.chars_dict_to_sorted_list(char_dict)
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {path}...")
+print("----------- Word Count ----------")
+print(f"Found {words_count} total words")
+print("--------- Character Count -------")
+
+for char_dict in chars_sorted_list:
+    char = char_dict["char"]
+    count = char_dict["num"]
+    if char.isalpha():
+        print(f"{char}: {count}")
+
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -3,8 +3,32 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
-def get_num_words():
-    contents = get_book_text("books/frankenstein.txt")
-    words = contents.split()
+def get_num_words(text):
+    words = text.split()
     words_count = len(words)
     return words_count
+
+def get_num_chars(text):
+    contents_lower = str.lower(text)
+    char_count = {}
+    for char in contents_lower:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+# In stats.py
+def sort_on(dict):
+    return dict["num"]
+
+def chars_dict_to_sorted_list(chars_dict):
+    # Create a list of dictionaries from the chars_dict
+    chars_list = []
+    for char, count in chars_dict.items():
+        chars_list.append({"char": char, "num": count})
+    
+    # Sort the list using the sort_on function
+    chars_list.sort(reverse=True, key=sort_on)
+    
+    return chars_list
